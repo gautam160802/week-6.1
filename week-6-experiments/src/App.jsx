82e169e483816dcabadbb49280600f4b1075be89
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-06-06 18:49:31 +0530

    add memo

--- week-6-experiments/src/App.jsx
+++ week-6-experiments/src/App.jsx
@@ -1,14 +1,63 @@
+// import { useState } from "react";
+
+// function App() {
+//   return (
+//     <div>
+//       <HeaderWithButton />
+//       <Header title="Harkirat1"></Header>
+//       <Header title="Harkirat2"></Header>
+//       <Header title="Harkirat2"></Header>
+//       <Header title="Harkirat2"></Header>
+//       <Header title="Harkirat2"></Header>
+//     </div>
+//   );
+// }
+
+// function HeaderWithButton() {
+//   const [title, setTitle] = useState("my name is gautam");
+//   function updateTitle() {
+//     setTitle("my name is" + Math.random());
+//   }
+//   return (
+//     <div>
+//       <button onClick={updateTitle}>Update the title</button>
+//       <Header title={title}></Header>
+//     </div>
+//   );
+// }
+
+// function Header({ title }) {
+//   return <div>{title}</div>;
+// }
+
+// export default App;
+
+// use memo
+
+import React, { useState } from "react";
+
 function App() {
+  const [title, setTitle] = useState("my name is harkirat");
+  function updateTitle() {
+    setTitle("my name is " + Math.random());
+  }
+
   return (
     <div>
-      <Header title="Harkirat1"></Header>
-      <Header title="Harkirat2"></Header>
+      <button onClick={updateTitle}>Update the title</button>
+      <Header title={title}></Header>
+      <Header title={title}></Header>
+      <Header title="harkirat2"></Header>
+      <Header title="harkirat2"></Header>
+      <Header title="harkirat2"></Header>
+      <Header title="harkirat2"></Header>
+      <Header title="harkirat2"></Header>
     </div>
   );
 }
 
-function Header({ title }) {
+const Header = React.memo(function Header({ title }) {
   return <div>{title}</div>;
-}
+});
 
 export default App;
