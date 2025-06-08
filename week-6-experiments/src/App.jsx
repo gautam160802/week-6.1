e855b5809197a7ab475fcf51dbcc73d6e483a952
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-06-08 13:39:25 +0530

    finally complete week 6.1

--- week-6-experiments/src/App.jsx
+++ week-6-experiments/src/App.jsx
@@ -149,23 +149,33 @@
 
 // real wrapper
 
-function App() {
-  return (
-    <div>
-      <CardWrapper>
-        <div>hi there</div>
-      </CardWrapper>
-      <CardWrapper>
-        <div>hello there</div>
-      </CardWrapper>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div>
+//       <CardWrapper>
+//         <CardWrapper>
+//           <CardWrapper>
+//             <div>hi there</div>
+//           </CardWrapper>
+//         </CardWrapper>
+//         <CardWrapper>
+//           <CardWrapper>
+//             <div>hello there</div>
+//           </CardWrapper>
+//         </CardWrapper>
+//       </CardWrapper>
+//     </div>
+//   );
+// }
 
-function CardWrapper({ children }) {
-  return (
-    <div style={{ border: "2px solid black", padding: 20 }}>{children}</div>
-  );
-}
+// function CardWrapper({ children }) {
+//   return (
+//     <div style={{ border: "2px solid black", padding: 20 }}>{children}</div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+// useEffect
+
+// Hooks
